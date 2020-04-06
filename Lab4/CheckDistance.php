cf6052fcd49c1a6a20bdb6c075c16b870e59c074
Tuong Phan <tramphongsts@gmail.com>
2020-04-06 14:06:39 +0700

    Tuong commits lab4's homework.

--- Lab4/CheckDistance.php
+++ Lab4/CheckDistance.php
@@ -23,7 +23,7 @@
             print "From Chicago to:<br>";
             
             ?>
-        <table>
+        <table border="1">
             <tr>
                 <td>No.</td>
                 <td>Destination</td>
@@ -33,19 +33,41 @@
             </tr>
         
         <?php
+        $i = 1;
                 foreach ($destination as $city){
+                    ?>
+            <tr>
+                
+            
+            <?php 
                     //print "city : $city<br>";
                     if(isset($cities[$city])){
                         $distance = $cities[$city];
                         $time = round( ($distance / 60), 2);
                         $walktime = round ( ($distance / 5), 2);
-                        print "The distance between Chicago and <I>$city</I> is $distance miles.";
-                        print "<br>Driving at 60 miles per hour it would take $time hours.";
-                        print "<br>Walking at 5 miles per hour it would take $walktime hours.";
+                        
+                        ?>
+                <td><?php echo $i?></td>
+                <td><?php echo $city?></td>
+                <td><?php echo $distance?></td>
+                <td><?php echo $time?></td>
+                <td><?php echo $walktime?></td>
+                <?php
+//                        print "The distance between Chicago and <I>$city</I> is $distance miles.";
+//                        print "<br>Driving at 60 miles per hour it would take $time hours.";
+//                        print "<br>Walking at 5 miles per hour it would take $walktime hours.";
                     }else {
-                        print "Sorry, do not have destination information for $destination.";
+                        echo "<td>$i</td>";
+                        echo "<td>$city</td>";
+                        echo "<td>No information</td>";
+//                        print "Sorry, do not have destination information for $destination.";
                     }
+                    $i++;
                 }
+                ?>
+            </tr>
+            
+            <?php
                 //print "$cities[$destination]";
            // }
         ?>
