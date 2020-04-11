6a64aeeb80327360374972dccb8724b0fe87f52b
Tuong Phan <tramphongsts@gmail.com>
2020-04-11 18:06:49 +0700

    Tuong commits lab6's homework: business service.

--- Lab6-1/business_service/admin.php
+++ Lab6-1/business_service/admin.php
@@ -1,8 +1,26 @@
 <html>
     <head>
         <?php
-        require_once('db_login.php');
-        ?>
+# parameters for connecting to the "business_service" 
+        $username = "root";
+        $password = "";
+        $hostspec = "localhost";
+        $database = "business_service";
+// $dbtype = 'pgsql';
+// $dbtype = 'oci8';
+        $dbtype = 'mysqli';
+
+# DSN constructed from parameters 
+        $dsn = "$dbtype://$username:$password@$hostspec/$database";
+
+# Establish the connection
+#$db = DB::connect($dsn);
+        $connect = mysqli_connect($hostspec, $username, $password);
+
+        if (!$connect) {
+            die($connect->getMessage());
+        }
+        ?> 
 
         <title>
             <?php
@@ -22,38 +40,53 @@
         <?php
         // add category record input section
         // extract values from $_REQUEST
-        $Cat_ID = $_REQUEST['Cat_ID'];
-        $Cat_Title = $_REQUEST['Cat_Title'];
-        $Cat_Desc = $_REQUEST['Cat_Desc'];
-        $add_record = $_REQUEST['add_record'];
+        if (isset($_GET['Cat_ID'])) {
+            $Cat_ID = $_GET['Cat_ID'];
+            $Cat_Title = $_GET['Cat_Title'];
+            $Cat_Desc = $_REQUEST['Cat_Desc'];
+            $add_record = $_REQUEST['add_record'];
+            //$connect = $_GET['connect'];
+            // determine the length of each input field
+            $len_cat_id = strlen($_REQUEST['Cat_ID']);
+            $len_cat_tl = strlen($_REQUEST['Cat_Title']);
+            $len_cat_de = strlen($_REQUEST['Cat_Desc']);
 
-        // determine the length of each input field
-        $len_cat_id = strlen($_REQUEST['Cat_ID']);
-        $len_cat_tl = strlen($_REQUEST['Cat_Title']);
-        $len_cat_de = strlen($_REQUEST['Cat_Desc']);
-
-        // validate and insert if the form script has been
-        // called by the Add Category button
-        if ($add_record == 1) {
-            if (($len_cat_id > 0) and ($len_cat_tl > 0) and ($len_cat_de > 0)) {
-                $sql = "insert into categories (category_id, title, description)";
-                $sql .= " values ('$Cat_ID', '$Cat_Title', '$Cat_Desc')";
-                $result = $db->query($sql);
-                $db->commit();
-            } else {
-                echo "<p>Please make sure all fields are filled in ";
-                echo "and try again.</p>\n";
+            // validate and insert if the form script has been
+            // called by the Add Category button
+            if ($add_record == 1) {
+                if (($len_cat_id > 0) and ($len_cat_tl > 0) and ($len_cat_de > 0)) {
+                    $sql = "insert into categories";
+                    $sql .= " values ('$Cat_ID', '$Cat_Title', '$Cat_Desc')";
+                    //print "$connect";
+                    mysqli_select_db($connect, $database);
+                    $result = mysqli_query($connect, $sql);
+                    if ($result) {
+                        print 'Inserted successfully';
+                    } else {
+                        print "Fail to insert";
+                    }
+                    //$db->commit();
+                } else {
+                    echo "<p>Please make sure all fields are filled in ";
+                    echo "and try again.</p>\n";
+                }
             }
         }
-
         // list categories reporting section
-        // query all records in the table after any
-        // insertion that may have occurred above
+// query all records in the table after any
+// insertion that may have occurred above
+        mysqli_select_db($connect, $database);
         $sql = "select * from categories";
-        $result = $db->query($sql);
+//$result = $db->query($sql);
+        $result = mysqli_query($connect, $sql);
+        if ($result) {
+            
+        } else {
+            print 'Fail to select';
+        }
         ?>
 
-        <form method="post" action="<?= $PHP_SELF ?>">
+        <form method="get" action="<?php echo $_SERVER['PHP_SELF'] ?>">
 
             <table>
                 <tr><th bgcolor="#eeeeee">Cat ID</th>
@@ -61,13 +94,14 @@
                     <th bgcolor="#eeeeee">Description</th>
                 </tr>
 
-<?php
+                <?php
 // display any records fetched from the database
 // plus an input line for a new category
-while ($row = $result->fetchRow()) {
-    echo "<tr><td>$row[0]</td><td>$row[1]</td><td>$row[2]</td></tr>\n";
-}
-?>
+
+                while ($row = mysqli_fetch_row($result)) {
+                    echo "<tr><td>$row[0]</td><td>$row[1]</td><td>$row[2]</td></tr>\n";
+                }
+                ?>
                 <tr><td><input type="text" name="Cat_ID"    size="15" maxlength="10" /></td>
                     <td><input type="text" name="Cat_Title" size="40" maxlength="128" /></td>
                     <td><input type="text" name="Cat_Desc"  size="45" maxlength="255" /></td>
@@ -75,5 +109,6 @@ while ($row = $result->fetchRow()) {
             </table>
             <input type="hidden" name="add_record" value="1" />
             <input type="submit" name="submit" value="Add Category" />
+        </form>
     </body>
 </html>
