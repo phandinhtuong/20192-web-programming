b649f9413d823acbba6c68c62709564e79f5af32
Tuong Phan <tramphongsts@gmail.com>
2020-04-14 10:49:25 +0700

    Tuong commits lab7's work.

---
+++ Lab7/DateProcessing.php
@@ -0,0 +1,26 @@
+<html>
+    <head><title>Date Check</title></head>
+    <body>
+        <?php
+            $date = $_POST['date'];
+            $two = '[[:digit:]]{2}';
+            $month = '[0-1][[:digit:]]';
+            $day = '[0-3][[:digit:]]';
+            $year = "2[[:digit:]]$two";
+            if(preg_match("/^$month\/$day\/$year$/", $date)){
+                list($mon, $day, $year) = preg_split('/\//', $date);
+                if ($mon>=1 && $mon<=12){
+                    if ($day<=31){
+                        print "Valid date mon=$mon, day=$day, year=$year, full date = $date<br>";
+                    } else {
+                        print 'Illegal day specified day = $day';
+                    }
+                } else {
+                    print 'Illegal month specified mon = $mon';
+                }
+            } else {
+                print "Invalid date format = $date";
+            }
+        ?>
+    </body>
+</html>
